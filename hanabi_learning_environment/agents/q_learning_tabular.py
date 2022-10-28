3dc1df85fabf8b0f6e2122d63697a5df5329e1a7
moonear2000 <mounir.hinedi@gmail.com>
2022-10-28 14:56:37 +0100

    begun looking at tabular approach

--- hanabi_learning_environment/agents/q_learning_tabular.py
+++ hanabi_learning_environment/agents/q_learning_tabular.py
@@ -2,9 +2,19 @@
 # Note this may be infeasable in most scenarios due to the large number of states.
 # This aims to solve the very-small hanabi game
 
+# Game parameters:
+## 1 colour
+## 5 ranks
+## 10 cards (000 11 22 33 4)
+## 2 players
+## Hand size = 2
+## 3 max hints
+## 1 max life
+
 # State consists off (in very small game):
-## cards observed in other players hand
+## Cards observed in other players hand
 ## Known cards in your hand (each card takes 1 hint)
-## cards in discard pile
-## firework card
-##  
+## Cards in discard pile
+## Firework card
+## Number of info tokens (3 max)
+## Number of life tokens (1 max)
